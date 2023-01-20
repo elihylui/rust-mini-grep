25ec1567d79bacbbc1874897242cf43353fead0f
Elizabeth Lui <95862857+elihylui@users.noreply.github.com>
2023-01-20 18:32:03 +0100

    add tests (#1)

---
+++ README.md
@@ -0,0 +1,3 @@
+## Simple Grep CLI Application in Rust
+
+This repo contains a small grep CLI app which takes query and filename as parameters and search for the query in the text file from the command line. It can also read the environment variable ```CASE_INSENSITIVE``` to make the search case-insensitive. This project follows the instructions in Ch.12 of the Rust Lang Book.  
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process; //allow us to exit process without panic
-use std::error::Error;
+
+use rust_mini_grep::Config;
 
 //main.rs is binary crate; lib.rs is library crate
 fn main() {
@@ -9,46 +9,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err|{
-        println!("Problem parsing argument: {}", err);
+        eprintln!("Problem parsing argument: {}", err);
         process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+    if let Err(e) = rust_mini_grep::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text: \n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    //constructor function - named as new() as a convention
-    fn new(args: &[String]) -> Result<Config, &str> {
-        //error handling:
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        //takes 2 arguments: query & filename
-        let query = args[1].clone(); //&args[0] is path to binaries
-        let filename = args[2].clone();
-    
-        Ok(Config { query, filename })
-    }
-    
-}
-
